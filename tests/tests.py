45bf858ecc9319b8243dcbf2eadb6130a47334e4
Alexandre Varas <alej0varas@gmail.com>
2017-01-31 17:08:51 +0100

    All methods implemented

--- tests/tests.py
+++ tests/tests.py
@@ -6,15 +6,12 @@ import requests
 import yaosac
 
 
-class ClientTestCase(unittest.TestCase):
+class ClientInternalTestCase(unittest.TestCase):
     key_name = 'OS_TEST_KEY'
     attr_name = 'test_key'
 
     def setUp(self):
-        self.client = yaosac.Client()
-        requests.get = mock.Mock()
-        requests.post = mock.Mock()
-        requests.delete = mock.Mock()
+        yaosac.client = yaosac.Client()
 
     def test_check_an_auth_key(self):
         """Check if an auth key is set"""
@@ -22,60 +19,108 @@ class ClientTestCase(unittest.TestCase):
         other_key = 'other-key'
         os.environ[self.key_name] = key
 
-        self.client._check_an_auth_key(self.key_name)
+        yaosac.client._check_an_auth_key(self.key_name)
 
-        self.assertEqual(getattr(self.client, '_' + self.attr_name), key)
+        self.assertEqual(getattr(yaosac.client, '_' + self.attr_name), key)
 
         del(os.environ[self.key_name])
 
     def test_check_an_auth_key__raises_improperlyconfigured(self):
         """If the auth key is not found raise an exception"""
         with self.assertRaises(Exception) as context:
-            self.client._check_an_auth_key(self.key_name)
+            yaosac.client._check_an_auth_key(self.key_name)
 
         self.assertIn(self.key_name, context.exception.args[0])
 
     def test_set_an_auth_key(self):
         value = 'a-value'
 
-        self.client._set_an_auth_key(self.key_name, value)
+        yaosac.client._set_an_auth_key(self.key_name, value)
 
-        self.assertEqual(getattr(self.client, '_' + self.attr_name), value)
+        self.assertEqual(getattr(yaosac.client, '_' + self.attr_name), value)
 
     def test_auth_key_properties(self):
-        value = 'a-value'
+        value = 'a-value-app'
         os.environ['OS_APP_AUTH_KEY'] = value
 
-        result = self.client.app_auth_key
+        result = yaosac.client.app_auth_key
 
-        self.assertEqual(self.client._app_auth_key, value)
+        self.assertEqual(yaosac.client._app_auth_key, value)
         self.assertEqual(result, value)
 
+        value = 'a-value-user'
         os.environ['OS_USER_AUTH_KEY'] = value
 
-        result = self.client.user_auth_key
+        result = yaosac.client.user_auth_key
 
-        self.assertEqual(self.client._user_auth_key, value)
+        self.assertEqual(yaosac.client._user_auth_key, value)
+        self.assertEqual(result, value)
+
+        value = 'a-value-app-id'
+        os.environ['OS_APP_ID'] = value
+
+        result = yaosac.client.app_id
+
+        self.assertEqual(yaosac.client._app_id, value)
         self.assertEqual(result, value)
 
     def test_auth_key_properties__setter(self):
-        value = 'a-key'
+        value = 'a-value-app'
 
-        self.client.app_auth_key = value
+        yaosac.client.app_auth_key = value
 
-        self.assertEqual(self.client._app_auth_key, value)
+        self.assertEqual(yaosac.client._app_auth_key, value)
 
-        self.client.user_auth_key = value
+        value = 'a-value-user'
 
-        self.assertEqual(self.client._user_auth_key, value)
+        yaosac.client.user_auth_key = value
+
+        self.assertEqual(yaosac.client._user_auth_key, value)
+
+        value = 'a-value-app-id'
+
+        yaosac.client.app_id = value
+
+        self.assertEqual(yaosac.client._app_id, value)
 
     def test_get_header(self):
-        key = 'test-key'
-        yaosac.client.test_auth_key = key
+        result = yaosac.client._get_headers()
+
+        self.assertIn('content-type', result)
+
+        value = 'a-value-test'
+        yaosac.client.test_auth_key = value
 
         result = yaosac.client._get_headers('test')
         
-        self.assertIn(key, result['authorization'])
+        self.assertIn(value, result['authorization'])
+
+        value = 'a-value-app'
+        yaosac.client._set_an_auth_key('APP_AUTH_KEY', value)
+
+        result = yaosac.client._get_headers()
+        
+        self.assertIn(value, result['authorization'])
+
+
+class ClientAPIMEthodsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app_auth_key = 'app-test-key'
+        self.app_id = 'app-test-id'
+        self.user_auth_key = 'user-test-key'
+        os.environ['OS_APP_AUTH_KEY'] = self.app_auth_key
+        os.environ['OS_APP_ID'] = self.app_id
+        os.environ['OS_USER_AUTH_KEY'] = self.user_auth_key
+        self.client = yaosac.Client()
+        requests.get = mock.Mock()
+        requests.post = mock.Mock()
+        requests.put = mock.Mock()
+        requests.delete = mock.Mock()
+        requests.get.return_value = not None
+        requests.post.return_value = not None
+        requests.put.return_value = not None
+        requests.delete.return_value = not None
+
     #
     # end-points tests
     #
@@ -86,8 +131,6 @@ class ClientTestCase(unittest.TestCase):
 
     def test_create_notification(self):
         kwargs = {'what': 'ever'}
-        expected_kwargs = kwargs.copy()
-        expected_kwargs.update({'headers': ''})
         contents = {'en': 'Bla'}
 
         response = self.client.create_notification(contents, **kwargs)
@@ -95,10 +138,11 @@ class ClientTestCase(unittest.TestCase):
         self.assertEqual(requests.post.call_count, 1)
         self.assertIn('contents', requests.post.call_args[0][0])
         self.assertIn('app_id', requests.post.call_args[0][0])
-        for key in kwargs:
-            self.assertIn(key, requests.post.call_args[1], key)
+        self.assertIn('what', requests.post.call_args[0][0])
         self.assertIn('headers', requests.post.call_args[1])
-        self.assertIsNone(response)
+        self.assertIn(self.app_auth_key,
+                      requests.post.call_args[1]['headers']['authorization'])
+        self.assertIsNotNone(response)
 
     def test_cancel_notification(self):
         id = 'notification-id'
@@ -109,36 +153,223 @@ class ClientTestCase(unittest.TestCase):
         self.assertEqual(requests.delete.call_count, 1)
         self.assertIn(id, requests.delete.call_args[0][0])
         self.assertIn(app_id, requests.delete.call_args[0][0])
-        self.assertIsNone(response)
+        self.assertIn('headers', requests.delete.call_args[1])
+        self.assertIn(self.app_auth_key,
+                      requests.delete.call_args[1]['headers']['authorization'])
+        self.assertIsNotNone(response)
+
+    def test_view_apps(self):
+        response = self.client.view_apps()
+
+        self.assertIn('apps', requests.get.call_args[0][0])
+        self.assertIn(self.user_auth_key,
+                      requests.get.call_args[1]['headers']['authorization'])
+        self.assertIsNotNone(response)
+
+    def test_view_an_app(self):
+        app_id = 'an-id'
+        response = self.client.view_an_app(app_id)
+
+        self.assertIn('apps', requests.get.call_args[0][0])
+        self.assertIn(app_id, requests.get.call_args[0][0])
+        self.assertIn(self.user_auth_key,
+                      requests.get.call_args[1]['headers']['authorization'])
+        self.assertIsNotNone(response)
+
+    def test_create_an_app(self):
+        payload = {'what': 'ever'}
+        response = self.client.create_an_app(**payload)
+
+        self.assertIn('apps', requests.post.call_args[0][0])
+        self.assertEqual(payload, requests.post.call_args[1]['data'])
+        self.assertIn(self.user_auth_key,
+                      requests.post.call_args[1]['headers']['authorization'])
+
+    def test_update_an_app(self):
+        payload = {'what': 'ever'}
+        app_id = ';P'
+        response = self.client.update_an_app(app_id, **payload)
+
+        self.assertIn(app_id, requests.put.call_args[0][0])
+        self.assertIn('apps', requests.put.call_args[0][0])
+        self.assertEqual(payload, requests.put.call_args[1]['data'])
+        self.assertIn(self.user_auth_key,
+                      requests.put.call_args[1]['headers']['authorization'])
+
+    def test_view_devices(self):
+        response = self.client.view_devices()
+
+        self.assertIn('players', requests.get.call_args[0][0])
+        self.assertIn('app_id=', requests.get.call_args[0][0])
+        self.assertIn(self.app_id, requests.get.call_args[0][0])
+        self.assertIn(self.app_auth_key,
+                      requests.get.call_args[1]['headers']['authorization'])
+
+        # with limit
+        requests.get.reset_mock()
+        limit = 69
+        response = self.client.view_devices(limit=limit)
+
+        self.assertIn('limit=', requests.get.call_args[0][0])
+        self.assertIn(str(limit), requests.get.call_args[0][0])
+
+        # with offset
+        requests.get.reset_mock()
+        offset = 420
+        response = self.client.view_devices(offset=offset)
+
+        self.assertIn('offset=', requests.get.call_args[0][0])
+        self.assertIn(str(offset), requests.get.call_args[0][0])
+
+    def test_view_device(self):
+        device_id = 'my-phone'
+        response = self.client.view_device(device_id)
+
+        self.assertIn('players', requests.get.call_args[0][0])
+        self.assertIn(device_id, requests.get.call_args[0][0])
+        self.assertIn('app_id=', requests.get.call_args[0][0])
+        self.assertIn(self.app_id, requests.get.call_args[0][0])
+        self.assertNotIn('authorization', requests.get.call_args[1]['headers'])
+
+    def test_add_a_device(self):
+        payload = {'read': 'the', 'docs': 1}
+        response = self.client.add_a_device(**payload)
+
+        self.assertIn('players', requests.post.call_args[0][0])
+        self.assertIn('app_id', requests.post.call_args[1]['data'])
+        self.assertEqual(self.app_id,
+                         requests.post.call_args[1]['data']['app_id'])
+        for key, value in payload.items():
+            self.assertIn(key, requests.post.call_args[1]['data'])
+            self.assertEqual(value, requests.post.call_args[1]['data'][key])
+        self.assertNotIn('authorization', requests.post.call_args[1]['headers'])
+
+    def test_edit_device(self):
+        device_id = 'my-phone'
+        payload = {'read': 'the', 'docs': 1}
+        response = self.client.edit_device(device_id, **payload)
+
+        self.assertIn('players', requests.put.call_args[0][0])
+        self.assertIn(device_id, requests.put.call_args[0][0])
+        self.assertIn('app_id', requests.put.call_args[1]['data'])
+        self.assertEqual(self.app_id,
+                         requests.put.call_args[1]['data']['app_id'])
+        for key, value in payload.items():
+            self.assertIn(key, requests.put.call_args[1]['data'])
+            self.assertEqual(value, requests.put.call_args[1]['data'][key])
+        self.assertNotIn('authorization', requests.put.call_args[1]['headers'])
+
+    def test_edit_device(self):
+        device_id = 'my-phone'
+        payload = {'read': 'the', 'docs': 1}
+        response = self.client.new_session(device_id, **payload)
+
+        self.assertIn('players', requests.post.call_args[0][0])
+        self.assertIn('on_session', requests.post.call_args[0][0])
+        self.assertIn(device_id, requests.post.call_args[0][0])
+        self.assertEqual(payload, requests.post.call_args[1]['data'])
+        self.assertNotIn('authorization', requests.post.call_args[1]['headers'])
+
+    def test_new_purchase(self):
+        device_id = 'my-phone'
+        payload = {'read': 'the', 'docs': 1}
+        response = self.client.new_purchase(device_id, **payload)
+
+        self.assertIn('players', requests.post.call_args[0][0])
+        self.assertIn('on_purchase', requests.post.call_args[0][0])
+        self.assertIn(device_id, requests.post.call_args[0][0])
+        self.assertEqual(payload, requests.post.call_args[1]['data'])
+        self.assertNotIn('authorization', requests.post.call_args[1]['headers'])
+
+    def test_increment_session_length(self):
+        device_id = 'my-phone'
+        active_time = 10
+        expected_payload = {'state': 'ping', 'active_time': active_time}
+
+        response = self.client.increment_session_length(device_id, active_time)
+
+        self.assertIn('players', requests.post.call_args[0][0])
+        self.assertIn('on_focus', requests.post.call_args[0][0])
+        self.assertIn(device_id, requests.post.call_args[0][0])
+        self.assertEqual(expected_payload, requests.post.call_args[1]['data'])
+        self.assertNotIn('authorization', requests.post.call_args[1]['headers'])
+
+    def test_csv_export(self):
+        response = self.client.csv_export()
+
+        self.assertIn('players', requests.post.call_args[0][0])
+        self.assertIn('csv_export', requests.post.call_args[0][0])
+        self.assertIn('app_id=', requests.post.call_args[0][0])
+        self.assertIn(self.app_id, requests.post.call_args[0][0])
+        self.assertIn(self.app_auth_key,
+                      requests.post.call_args[1]['headers']['authorization'])
 
 
-class AddHeadersTestCase(unittest.TestCase):
-    def setUp(self):
-        self._orgi_app = yaosac.APP_AUTH_METHODS
-        self._orgi_user = yaosac.USER_AUTH_METHODS
-        
-    def test_cases_for_app_auth_key(self):
-        os.environ['OS_APP_AUTH_KEY'] = 'a-key'
-        method_name = 'app_auth_method'
-        yaosac.APP_AUTH_METHODS = [method_name, ]
-        f = mock.Mock()
-        f.__name__ = method_name
+        # Extra fields
+        # location
+        requests.post.reset_mock()
+        response = self.client.csv_export(location=True)
 
-        yaosac.add_headers(f)(yaosac.client)
+        self.assertIn('location',
+                      requests.post.call_args[1]['data']['extra_fields'])
+        # country
+        requests.post.reset_mock()
+        response = self.client.csv_export(country=True)
 
-        f.assert_called_once_with(yaosac.client, headers=yaosac.client._get_headers('app'))
+        self.assertIn('country',
+                      requests.post.call_args[1]['data']['extra_fields'])
 
-    def test_cases_for_user_auth_key(self):
-        os.environ['OS_USER_AUTH_KEY'] = 'user-key'
-        method_name = 'user_auth_method'
-        yaosac.USER_AUTH_METHODS = [method_name, ]
-        f = mock.Mock()
-        f.__name__ = method_name
+        # rooted
+        requests.post.reset_mock()
+        response = self.client.csv_export(rooted=True)
 
-        yaosac.add_headers(f)(yaosac.client)
+        self.assertIn('rooted',
+                      requests.post.call_args[1]['data']['extra_fields'])
 
-        f.assert_called_once_with(yaosac.client, headers=yaosac.client._get_headers(auth_name='user'))
+    def test_view_notification(self):
+        notification_id = 'an-push'
+        response = self.client.view_notification(notification_id)
 
-    def tearDown(self):
-        yaosac.APP_AUTH_METHODS = self._orgi_app
-        yaosac.USER_AUTH_METHODS = self._orgi_user
+        self.assertIn('notifications', requests.get.call_args[0][0])
+        self.assertIn(notification_id, requests.get.call_args[0][0])
+        self.assertIn('app_id=', requests.get.call_args[0][0])
+        self.assertIn(self.app_id, requests.get.call_args[0][0])
+        self.assertIn(self.user_auth_key,
+                      requests.get.call_args[1]['headers']['authorization'])
+
+    def test_view_notifications(self):
+        response = self.client.view_notifications()
+
+        self.assertIn('notifications', requests.get.call_args[0][0])
+        self.assertIn('app_id=', requests.get.call_args[0][0])
+        self.assertIn(self.app_id, requests.get.call_args[0][0])
+        self.assertIn(self.app_auth_key,
+                      requests.get.call_args[1]['headers']['authorization'])
+
+        # with limit
+        requests.get.reset_mock()
+        limit = 69
+        response = self.client.view_notifications(limit=limit)
+
+        self.assertIn('limit=', requests.get.call_args[0][0])
+        self.assertIn(str(limit), requests.get.call_args[0][0])
+
+        # with offset
+        requests.get.reset_mock()
+        offset = 420
+        response = self.client.view_notifications(offset=offset)
+
+        self.assertIn('offset=', requests.get.call_args[0][0])
+        self.assertIn(str(offset), requests.get.call_args[0][0])
+
+    def test_track_open(self):
+        notification_id = 'a-push'
+        response = self.client.track_open(notification_id)
+
+        self.assertIn('notifications', requests.put.call_args[0][0])
+        self.assertIn(notification_id, requests.put.call_args[0][0])
+        self.assertIn('app_id', requests.put.call_args[1]['data'])
+        self.assertIn(self.app_id, requests.put.call_args[1]['data']['app_id'])
+        self.assertIn('opened', requests.put.call_args[1]['data'])
+        self.assertTrue(requests.put.call_args[1]['data']['opened'])
+        self.assertNotIn('authorization', requests.put.call_args[1]['headers'])
--- yaosac.py
+++ yaosac.py
@@ -6,31 +6,19 @@ import requests
 class ImproperlyConfigured(Exception):
     pass
 
-APP_AUTH_METHODS = ['create_notification', ]
-USER_AUTH_METHODS = ['view_apps', ]
-
-
-def add_headers(f):
-    """Decorator that adds the corresponding header to each method"""
-
-    def deco(self, *args, **kwargs):
-        if f.__name__ in APP_AUTH_METHODS:
-            auth = 'app'
-        elif f.__name__ in USER_AUTH_METHODS:
-            auth = 'user'
-        kwargs.update({'headers': client._get_headers(auth)})
-        f(self, *args, **kwargs)
-    return deco
-
-
 
 class Client:
-    """To use the client you need an User Auth Key or an App Auth Key[0]
-    depending on the methods you want to call. They can be set as
-    environment variables 'OS_USER_AUTH_KEY' and 'OS_APP_AUTH_KEY' or
-    assigned to the client via `app_auth_key` and `user_auth_key`.
+    """Client methods are a map of the server API end-points[0] where the
+    method name is the end-point name in lower case and spaces
+    replaced by underscores. Depending on the methods you call you
+    will need an User Auth Key or an App Auth Key or an App
+    Id[1]. They can be set as environment variables
+    'OS_USER_AUTH_KEY', 'OS_APP_AUTH_KEY' and 'OS_APP_ID' or assigned
+    to the client via `app_auth_key`, `user_auth_key` and `app_id`
+    attributes.
 
-    0 - https://documentation.onesignal.com/docs/accounts-and-keys
+    0 - https://documentation.onesignal.com/reference
+    1 - https://documentation.onesignal.com/docs/accounts-and-keys
 
     """
 
@@ -38,20 +26,22 @@ class Client:
 
     def _check_an_auth_key(self, key_name):
         value = os.environ.get(key_name, None)
+        attr_name = (key_name[3:] if key_name.startswith('OS_') else
+                     key_name).lower()
         if value is None:
             raise ImproperlyConfigured(("An Auth Key missing. You can set "
                                         "it through the '%s' environment "
-                                        "variable or the 'app_auth_key' "
-                                        "argument." % key_name))
-        self._set_an_auth_key(key_name, value)
+                                        "variable or the '%s' "
+                                        "argument." % (key_name, attr_name)))
+        self._set_an_auth_key(attr_name, value)
 
-    def _set_an_auth_key(self, key_name, value):
-        attr_name = key_name[3:] if key_name.startswith('OS_')  else key_name
+    def _set_an_auth_key(self, attr_name, value):
         setattr(self, '_' + attr_name.lower(), value)
 
     @property
     def app_auth_key(self):
-        self._check_an_auth_key('OS_APP_AUTH_KEY')
+        if getattr(self, '_app_auth_key', None) is None:
+            self._check_an_auth_key('OS_APP_AUTH_KEY')
         return self._app_auth_key
 
     @app_auth_key.setter
@@ -60,38 +50,189 @@ class Client:
             self._set_an_auth_key('OS_APP_AUTH_KEY', value)
 
     @property
+    def app_id(self):
+        if getattr(self, '_app_id', None) is None:
+            self._check_an_auth_key('OS_APP_ID')
+        return self._app_id
+
+    @app_id.setter
+    def app_id(self, value):
+        if getattr(self, '_app_id', None) is None:
+            self._set_an_auth_key('OS_APP_ID', value)
+
+    @property
     def user_auth_key(self):
-        self._check_an_auth_key('OS_USER_AUTH_KEY')
-        return self._app_auth_key
+        if getattr(self, '_user_auth_key', None) is None:
+            self._check_an_auth_key('OS_USER_AUTH_KEY')
+        return self._user_auth_key
 
     @user_auth_key.setter
     def user_auth_key(self, value):
         if getattr(self, '_user_auth_key', None) is None:
             self._set_an_auth_key('OS_USER_AUTH_KEY', value)
 
-    def _get_headers(self, auth_name):
-        auth = getattr(self, auth_name + '_auth_key')
-        headers = {'content-type': 'application/json',
-                   'authorization': 'Basic %s' % auth}
+    def _get_headers(self, auth_name=None):
+        headers = {'content-type': 'application/json'}
+        if auth_name is not None:
+            auth = getattr(self, auth_name + '_auth_key')
+            headers.update({'authorization': 'Basic %s' % auth})
         return headers
     #
     # API methods
     #
-    @add_headers
     def create_notification(self, contents, **kwargs):
         _url = 'notifications'
         url = self.OS_URL + _url
         data = {'contents': contents,
-                'app_id': self.app_auth_key}
+                'app_id': self.app_id}
         data.update(kwargs)
+        headers = self._get_headers('app')
 
-        response = requests.post(data, url, **kwargs)
+        response = requests.post(data, url, headers=headers)
+        return response
 
     def cancel_notification(self, id, app_id):
         _url = 'notifications'
         url = self.OS_URL + _url + '/' + id + '?app_id=' + app_id
+        headers = self._get_headers('app')
 
-        response = requests.delete(url)
+        response = requests.delete(url, headers=headers)
+        return response
+
+    def view_apps(self):
+        _url = 'apps'
+        url = self.OS_URL + _url
+        headers = self._get_headers('user')
+
+        response = requests.get(url, headers=headers)
+        return response
+        
+    def view_an_app(self, app_id):
+        _url = 'apps'
+        url = self.OS_URL + _url + '/' + app_id
+        headers = self._get_headers('user')
+
+        response = requests.get(url, headers=headers)
+        return response
+
+    def create_an_app(self, **kwargs):
+        _url = 'apps'
+        url = self.OS_URL + _url
+        headers = self._get_headers('user')
+
+        response = requests.post(url, data=kwargs, headers=headers)
+        return response
+        
+    def update_an_app(self, app_id, **kwargs):
+        _url = 'apps'
+        url = self.OS_URL + _url + '/' + app_id
+        headers = self._get_headers('user')
+
+        response = requests.put(url, data=kwargs, headers=headers)
+        return response
+
+    def view_devices(self, limit=None, offset=None):
+        _url = 'players'
+        url = self.OS_URL + _url + '/?app_id=' + self.app_id
+        if limit is not None:
+            url += '&limit=' + str(limit)
+        if offset is not None:
+            url += '&offset=' + str(offset)
+        headers = self._get_headers('app')
+
+        response = requests.get(url, headers=headers)
+        return response
+
+    def view_device(self, device_id):
+        _url = 'players'
+        url = self.OS_URL + _url + '/' + device_id + '?app_id=' + self.app_id
+        headers = self._get_headers()
+
+        response = requests.get(url, headers=headers)
+        return response
+
+    def add_a_device(self, **kwargs):
+        _url = 'players'
+        url = self.OS_URL + _url
+        kwargs.update({'app_id': self.app_id})
+        headers = self._get_headers()
+
+        response = requests.post(url, data=kwargs, headers=headers)
+        return response
+
+    def edit_device(self, device_id, **kwargs):
+        _url = 'players'
+        url = self.OS_URL + _url + '/' + device_id
+        kwargs.update({'app_id': self.app_id})
+        headers = self._get_headers()
+
+        response = requests.put(url, data=kwargs, headers=headers)
+        return response
+
+    def new_session(self, device_id, **kwargs):
+        _url = 'players'
+        url = self.OS_URL + _url + '/' + device_id + '/on_session'
+        headers = self._get_headers()
+
+        response = requests.post(url, data=kwargs, headers=headers)
+        return response
+
+    def new_purchase(self, device_id, **kwargs):
+        _url = 'players'
+        url = self.OS_URL + _url + '/' + device_id + '/on_purchase'
+        headers = self._get_headers()
+
+        response = requests.post(url, data=kwargs, headers=headers)
+        return response
+
+    def increment_session_length(self, device_id, active_time):
+        _url = 'players'
+        url = self.OS_URL + _url + '/' + device_id + '/on_focus'
+        data = {'state': 'ping', 'active_time': active_time}
+        headers = self._get_headers()
+
+        response = requests.post(url, data=data, headers=headers)
+        return response
+
+    def csv_export(self, **kwargs):
+        _url = 'players'
+        url = self.OS_URL + _url + '/csv_export' + '?app_id=' + self.app_id
+        data = {'extra_fields': list(kwargs.keys())}
+        headers = self._get_headers('app')
+
+        response = requests.post(url, data=data, headers=headers)
+        return response
+
+    def view_notification(self, notification_id):
+        _url = 'notifications'
+        url = (self.OS_URL + _url + '/' + notification_id
+               + '?app_id=' + self.app_id)
+        headers = self._get_headers('user')
+
+        response = requests.get(url, headers=headers)
+        return response
+
+    def view_notifications(self, limit=None, offset=None):
+        _url = 'notifications'
+        url = self.OS_URL + _url + '/' + '?app_id=' + self.app_id
+        if limit is not None:
+            url += '&limit=' + str(limit)
+        if offset is not None:
+            url += '&offset=' + str(offset)
+        headers = self._get_headers('app')
+
+        response = requests.get(url, headers=headers)
+        return response
+
+    def track_open(self, notification_id):
+        _url = 'notifications'
+        url = self.OS_URL + _url + '/' + notification_id
+        data = {'app_id': self.app_id,
+                'opened': True}
+        headers = self._get_headers()
+
+        response = requests.put(url, data=data, headers=headers)
+        return response
 
 
 client = Client()
